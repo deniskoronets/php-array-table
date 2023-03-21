c2515fc63bc81bfc5777e12867a4de0d8cf35a2c
8ctopus <hello@octopuslabs.io>
2023-03-21 14:42:45 +0400

    Update php cs fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,6 +7,21 @@ $finder = PhpCsFixer\Finder::create()
 $fixer = (new PhpCsFixer\Config('', ''))
     ->setRules([
         '@PhpCsFixer' => true,
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'global_namespace_import' => [
+            'import_classes' => false,
+            'import_constants' => false,
+            'import_functions' => true,
+        ],
+        'no_superfluous_phpdoc_tags' => false,
+        'phpdoc_add_missing_param_annotation' => true,
+        'phpdoc_no_empty_return' => false,
+        'phpdoc_summary' => false,
+        'phpdoc_trim' => true,
+        'phpdoc_trim_consecutive_blank_line_separation' => true,
+        'yoda_style' => false,
     ])
     ->setFinder($finder);
 
